26a06028f8602e9c2f6f13a2594524868c281d86
Nikita Belov <zodiac.nv@gmail.com>
2016-11-17 22:45:22 +0300

    Generator and tester.

---
+++ generator_diagonal.cpp
@@ -0,0 +1,39 @@
+#include <cctype>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("Usage: generator_diagonal <output.dat> <size>.\n");
+        return 1;
+    }
+    
+    for (int i = 0; i < strlen(argv[2]); i++) {
+        char ch = argv[2][i];
+        if (!isdigit(ch)) {
+            cerr << "Non digit symbol '" << ch << "' in string '" << argv[2] << "'." << endl;
+            return 2;
+        }
+    }
+    
+    int size = strtol(argv[2], NULL, 10);
+    if (size == 0) {
+        cerr << "Error converting '" << argv[2] << "' to int." << endl;
+        return 3;
+    }
+    
+    ofstream output(argv[1], ios::binary);
+    output.write(reinterpret_cast<const char *>(&size), sizeof(size));
+    output.write(reinterpret_cast<const char *>(&size), sizeof(size));
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int val = (i == j);
+            output.write(reinterpret_cast<const char *>(&val), sizeof(val));
+        }
+    }
+    output.close();
+}
---
+++ tester_diagonal.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int read_int(ifstream &input)
+{
+    int val;
+    input.read(reinterpret_cast<char *>(&val), sizeof(val));
+    return val;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("Usage: tester_diagonal <input.dat>.\n");
+        return 1;
+    }
+    
+    ifstream input(argv[1], ios::binary);
+    int size1 = read_int(input), size2 = read_int(input);
+    
+    if (size1 != size2) {
+        cerr << "Matrix is not diagonal: " << size1 << " x " << size2 << "." << endl;
+        return 2;
+    }
+    
+    for (int i = 0; i < size1; i++) {
+        for (int j = 0; j < size1; j++) {
+            int val = read_int(input);
+            
+            if (i == j && val != 1) {
+                cerr << "Matrix is not diagonal: " << val << " but expected 1 at (" << i << ", " << j << ")." << endl;
+                return 3;
+            } else if (i != j && val != 0) {
+                cerr << "Matrix is not diagonal: " << val << " but expected 0 at (" << i << ", " << j << ")." << endl;
+                return 4;
+            }
+        }
+    }
+    input.close();
+    
+    cout << "Matrix is correct." << endl;
+}
